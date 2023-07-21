f23170ea862270b97503b41eebef952594427f52
shidqi29 <shidqiamirulhaq@gmail.com>
2023-07-21 14:12:56 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="">Hello World</div>
+    </div>
+  )
+}
+
+export default App
