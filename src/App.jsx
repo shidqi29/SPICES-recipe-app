89b2035d0ca6d7d583953bd137dbcf0db73449cc
shidqi29 <shidqiamirulhaq@gmail.com>
2023-07-22 11:33:18 +0700

    configure layout and router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import HomePage from "./pages/home"
+import { Routes, Route, Outlet } from "react-router-dom";
+
+import { Fragment } from "react";
+import Footer from "./components/layouts/Footer";
+import Navbar from "./components/layouts/Navbar";
+import HomePage from "./pages/home";
+import RecipeDetail from "./pages/recipeDetail";
+
+const Layout = () => {
+  return (
+    <Fragment>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </Fragment>
+  );
+};
 
 const App = () => {
   return (
-    <main className="">
-      <HomePage />
-    </main>
-  )
-}
+    <div className="bg-black">
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route index element={<HomePage />} />
+        <Route path="recipes/:id" element={<RecipeDetail />} />
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
