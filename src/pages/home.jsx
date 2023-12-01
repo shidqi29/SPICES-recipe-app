c7eb36480824b1926e746278cd4d20ebf864f4c8
shidqi29 <shidqiamirulhaq@gmail.com>
2023-12-01 15:57:47 +0700

    fix title & icon

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,13 +5,12 @@ const HomePage = () => {
   return (
     <main className="w-full flex flex-col ">
       <HeroSection
-        title={<p>Unlock the Hidden Flavors of International Cuisines!</p>}
+        title={"Unlock the Hidden Flavors of International Cuisines"}
         type="home"
       />
       <section
         id="recipes"
-        className="md:max-w-[1440px] mx-auto px-4 md:px-20 mt-4"
-      >
+        className="md:max-w-[1440px] mx-auto px-4 md:px-20 mt-4">
         <Recipes />
       </section>
     </main>
